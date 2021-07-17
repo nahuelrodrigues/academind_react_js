46d6a49ee00eaf0c0bc124d45c66f2e3d5157bdf
Nahuel <rodriguesnahuel@gmail.com>
2021-07-17 04:39:56 -0300

    first module

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
-function ExpenseItem() {
+import ExpenseDate from "./ExpenseDate";
+import Card from './Card';
+import "./ExpenseItem.css";
+import React from "react";
+
+function ExpenseItem(props) {
   return (
-    <div>
-      <div>Date</div>
-      <div>
-        <h2>Title</h2>
-        <div>Amount</div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
