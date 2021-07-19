ad28542327029c9ca1265f00169684b56eb8e794
Nahuel <rodriguesnahuel@gmail.com>
2021-07-18 23:45:01 -0300

    Section 3

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
@@ -23,20 +23,20 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
-  // return (
-  //   <div>
-  //   <h2>Let's get started!</h2>
-  //   <Expenses items={expenses}/>
-  //   </div>
-  // );
+  return (
+    <div>
+    <h2>Let's get started!</h2>
+    <Expenses items={expenses}/>
+    </div>
+  );
 
   // alternativa a la anterior
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h2", {}, "Let's get started!"),
-    React.createElement(Expenses, { items: expenses })
-  );
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
 }
 
 export default App;
